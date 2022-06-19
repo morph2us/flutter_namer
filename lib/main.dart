d0c12a57e3a4c72234414444ae9596fffa5ff297
morph2us <architect.chun@gmail.com>
2022-06-20 01:55:44 +0900

    random word body text

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
+    final randomWord = WordPair.random();
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("naming app"),
         ),
+        body: Center(child: Text(randomWord.asPascalCase, textScaleFactor: 1.5,)),
       ),
     );
     // TODO: implement build
