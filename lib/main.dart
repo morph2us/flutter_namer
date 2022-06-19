e0ec32df5e058bd0b190d2fcb9477ec04bd05e32
morph2us <architect.chun@gmail.com>
2022-06-20 01:22:00 +0900

    add and appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("naming app"),
+        ),
+      ),
+    );
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  
+}
