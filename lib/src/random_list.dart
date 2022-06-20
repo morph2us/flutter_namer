fe7b581f07ef6a7130a441e8e5d4ae037e9c7e5a
morph2us <architect.chun@gmail.com>
2022-06-20 23:35:54 +0900

    ListView 로 만들고, 코드 정리

--- lib/src/random_list.dart
+++ lib/src/random_list.dart
@@ -1,28 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
-class RandomList extends StatefulWidget{
+class RandomList extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return _RandomListState();
-    throw UnimplementedError();
-  }
-
+  State<StatefulWidget> createState() => _RandomListState();
 }
 
-class _RandomListState extends State<RandomList>{
+class _RandomListState extends State<RandomList> {
+  final List<WordPair> _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = Set<WordPair>();
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    final randomWord = WordPair.random();
     return Scaffold(
       appBar: AppBar(
         title: Text("naming app"),
       ),
-      body: Center(child: Text(randomWord.asPascalCase, textScaleFactor: 1.5,)),
+      body: _buildList(),
     );
-    throw UnimplementedError();
   }
-  
+
+  Widget _buildList() {
+    return ListView.builder(itemBuilder: (context, index) {
+      // 0, 2, 4, 6, 8... = real items
+      // 1, 3, 5, 7, 9... = dividers
+
+      if (index.isOdd) {
+        return Divider();
+      }
+      var realIndex = index ~/ 2;
+      if (realIndex >= _suggestions.length) {
+        _suggestions.addAll(generateWordPairs().take(10));
+      }
+      return _buildRow(_suggestions[realIndex]);
+    });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(pair.asPascalCase, textScaleFactor: 1.5),
+      trailing: Icon(
+        Icons.favorite_border,
+        color: Colors.pink,
+      ),
+    );
+  }
 }
